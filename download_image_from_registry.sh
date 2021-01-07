8c9c0a8dc7a6eec3949e4c8a74286902324cb2c2
LoiND <danhloi2210@gmail.com>
2021-01-08 00:11:59 +0700

    add script download docker image to file

---
+++ download_image_from_registry.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+
+ARR_LIST_IMAGES=(  )
+add_image_to_list()
+{
+    ARR_LIST_IMAGES[${#ARR_LIST_IMAGES[@]}]=$1
+}
+
+read -p "Input image to download: " IMAGE
+add_image_to_list $IMAGE
+
+IS_CONTINUE_INPUT='Y'
+while [[ $IS_CONTINUE_INPUT != 'N' ]]; do
+    read -p "Do you want input more image? (image/N): " IS_CONTINUE_INPUT
+    if [[ $IS_CONTINUE_INPUT != 'N' ]]; then
+        add_image_to_list $IS_CONTINUE_INPUT
+    fi
+done
+
+for value in "${ARR_LIST_IMAGES[@]}"
+do
+     echo $value
+     docker pull $value
+     image_name=$(echo $value | sed -r "s#(.+)\/(.+$)#\2#g" | sed "s/:/-/").tar.gz
+     docker save $value | gzip > $image_name
+     echo "=================================="
+done
